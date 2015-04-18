f1d42cbdf1f74d66b51dd7116e134b7c060bf562
Azat Khuzhin <a3at.mail@gmail.com>
2015-04-18 19:23:04 +0300

    bisect: wrapper for git bisect run

---
+++ bisect.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+SELF=$(readlink -f "$(dirname $0)")
+export PATH=$PATH:$SELF
+source bootstrap.sh
+
+function run_bisect()
+{
+    local self="$SELF"
+    local sources="$1"
+    shift
+
+    cd "$sources"
+    git bisect start $@
+    git bisect run bisect-tester.sh $self $sources
+    git bisect log
+}
+
+function print_help()
+{
+    echo "$0 [ OPTS ] /path/to/linux-sources args-for-bisect" >&2
+    echo " -p     - prepare"
+    echo
+    echo " -h     - print this message"
+    exit 1
+}
+function parse_options()
+{
+    local OPTIND OPTARG o
+
+    while getopts "hpr:" o; do
+        case $o in
+            h) print_help ;;
+            p) prepare ;;
+        esac
+    done
+
+    shift $((OPTIND-1))
+    run_bisect $@
+}
+
+function main()
+{
+    parse_options $@
+}
+
+main $@
